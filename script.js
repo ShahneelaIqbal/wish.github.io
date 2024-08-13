8ae5c72151602d31d80d060a612c162684d65e2a
ShahneelaIqbal <120154520+ShahneelaIqbal@users.noreply.github.com>
2024-08-13 11:53:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const playButton = document.getElementById('playButton');
+
+const wishButton = document.getElementById('wishButton');
+const funnyWish = document.getElementById('funnyWish');
+
+const audio = new Audio('Chhote .mp3'); 
+
+playButton.addEventListener('click', () => {
+    audio.play();
+
+    // Hide the play button after the song starts playing
+    playButton.style.display = 'none';
+
+    setTimeout(() => {
+        wishButton.style.display = 'block';
+    }, 10000); 
+});
+
+wishButton.addEventListener('click', () => {
+    funnyWish.innerHTML = "To my amazing sister-in-law, who brings joy, laughter, and the best excuses for eating cake—have a fabulous birthday. Remember, calories do not count today. Lets celebrate with cake, laughter, and absolutely no talk about how old were getting!";
+    funnyWish.style.display = 'block';
+
+    wishButton.style.display = 'none';
+});
